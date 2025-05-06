3ac5e26b66a5f4901dc0907f9b9ed22c95eebc9c
Ivansyah <ivansyahsantoso@gmail.com>
2025-05-06 10:25:53 +0700

    repair layout

--- resources/views/components/student/dasbor/rekomendasi-magang.blade.php
+++ resources/views/components/student/dasbor/rekomendasi-magang.blade.php
@@ -1,7 +1,7 @@
 <h4 class="cursor-default mt-10 mb-5 text-2xl font-semibold text-[#5955b2]">
     Rekomendasi Magang
 </h4>
-<section class="grid grid-cols-1 sm:grid-cols-2 gap-6 max-h-[600px] w-full overflow-y-auto pr-2 lg:max-h-[450px]">
+<section class="max-h-[600px] max-w-full w-full overflow-y-auto pr-2 space-y-7 lg:max-h-[450px] lg:max-w-1/2">
     @foreach ($lowongan as $item)
         <x-card
             :category="$item->bidang_keahlian"
