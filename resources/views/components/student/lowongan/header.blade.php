1a666172ba74bfac2435ae3ac38c505bf552af22
Ivansyah <ivansyahsantoso@gmail.com>
2025-05-06 09:19:16 +0700

    create search column and button searching

---
+++ resources/views/components/student/lowongan/header.blade.php
@@ -0,0 +1,6 @@
+<section class="flex items-center justify-between">
+    <div class="flex flex-col">
+        <h4 class="text-2xl font-semibold text-[#5955b2] cursor-default">Lowongan</h4>
+    </div>
+    @include('shared.ui.profile-photo')
+</section>
--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,13 +1,36 @@
-<fieldset class="relative w-full mx-auto my-4">
-    <input
-        type="search"
-        id="search"
-        class="w-full py-3 pl-14 pr-4 text-sm border border-gray-300 bg-[#f9f8fe] rounded-full shadow focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-        placeholder="Cari sesuatu..."
-        wire:model.debounce.500ms="search"
-    />
-    <label for="search" class="sr-only">Search</label>
-    <span class="absolute inset-y-0 left-0 flex items-center pl-6 pointer-events-none text-gray-400">
-        <i class="fa-solid fa-magnifying-glass"></i>
-    </span>
-</fieldset>
+<div class="flex flex-wrap gap-4 w-full mx-auto my-4">
+    <div class="relative flex-1">
+        <input
+            type="search"
+            id="search"
+            class="w-full py-3 pl-14 pr-4 text-sm border border-gray-300 bg-[#f9f8fe] rounded-full shadow focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+            placeholder="Cari Nama Pekerjaan, Skill, Perusahaan"
+            wire:model.debounce.500ms="search"
+        />
+        <label for="search" class="sr-only">Search</label>
+        <span class="absolute inset-y-0 left-0 flex items-center pl-6 pointer-events-none text-gray-400">
+            <i class="fa-solid fa-magnifying-glass"></i>
+        </span>
+    </div>
+
+    <div class="relative w-64">
+        <input
+            type="search" 
+            id="location"
+            class="w-full py-3 pl-14 pr-4 text-sm border border-gray-300 bg-[#f9f8fe] rounded-full shadow focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+            placeholder="Lokasi"
+        />
+        <label for="location" class="sr-only">Location</label>
+        <span class="absolute inset-y-0 left-0 flex items-center pl-6 pointer-events-none text-gray-400">
+            <i class="fa-solid fa-location-dot"></i>
+        </span>
+    </div>
+    <div class="relative w-auto">
+        <button 
+            type="submit"
+            id="cari"
+            class="w-full py-3 px-6 text-sm text-white bg-[#E86BB1] rounded-full shadow hover:bg-pink-500 focus:outline-none focus:ring-2 focus:ring-pink-400 focus:border-pink-400"
+        >
+            Cari
+        </button>
+    </div>
