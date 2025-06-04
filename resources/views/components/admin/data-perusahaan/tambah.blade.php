0200a8b700fc5760d464e70ff3926ea64891c79e
Ivansyah <ivansyahsantoso@gmail.com>
2025-06-04 20:50:21 +0700

    ⏳ progress implementation pop up tambah Data Perusahaan

--- resources/views/components/admin/data-perusahaan/tambah.blade.php
+++ resources/views/components/admin/data-perusahaan/tambah.blade.php
@@ -1,4 +1,4 @@
-<section class="modal modal-tambah-dosen fixed inset-0 z-50 hidden items-center justify-center bg-black/50 backdrop-blur-sm" aria-modal="true" role="dialog">
+<section class="modal modal-tambah-perusahaan fixed inset-0 z-50 hidden items-center justify-center bg-black/50 backdrop-blur-sm" aria-modal="true" role="dialog">
     <div class="min-h-screen flex items-center justify-center w-full px-4">
         <form action="{{ route('admin.data-perusahaan.tambah') }}" method="POST" class="max-h-[90vh] overflow-y-auto w-full max-w-xl rounded-xl bg-white p-6 shadow-lg border border-[var(--stroke)]">
             @csrf
@@ -7,12 +7,9 @@
                 Tambah Perusahaan Mitra
             </h2>
             <hr class="mb-4 border border-[var(--primary)]" />
-            <h5 class="cursor-default mt-6 px-6 py-4 rounded-md text-sm bg-[var(--secondary)] text-white">
-                Data Pengguna
-            </h5>
-            <span class="mb-3 mt-6 flex items-center justify-between gap-4">
+            <div class="my-6 flex flex-col gap-3">
                 <x-input
-                    icon="fa-solid fa-user"
+                    icon="fa-solid fa-building"
                     label="Nama Perusahaan"
                     type="text"
                     name="nama"
@@ -20,40 +17,28 @@
                     :required="true"
                 />
                 <x-input
-                    icon="fa-solid fa-key"
-                    label="Kata Sandi"
-                    type="password"
-                    name="kata_sandi"
-                    placeholder="Masukkan Kata Sandi"
-                    :required="true"
-                />
-            </span>
-            <x-input
-                icon="fa-solid fa-envelope"
-                label="Email"
-                type="email"
-                name="email"
-                placeholder="Masukkan Email"
-                :required="true"
-            />
-            <h5 class="cursor-default my-6 px-6 py-4 rounded-md text-sm bg-[var(--secondary)] text-white">
-                Data Dosen
-            </h5>
-            <div class="my-6 flex flex-col gap-3">
-                <x-input
-                    icon="fa-solid fa-envelope"
-                    label="Nama Lengkap"
-                    type="text"
-                    name="nama"
-                    placeholder="Masukkan Nama Lengkap"
+                    icon="fa-solid fa-image"
+                    label="Gambar/Logo Perusahaan"
+                    type="file"
+                    name="logo"
+                    placeholder="Masukkan Logo"
                     :required="true"
                 />
                 <x-input
                     icon="fa-solid fa-id-card"
-                    label="NIP"
+                    label="NIB"
                     type="number"
-                    name="nip"
-                    placeholder="Masukkan NIP"
+                    name="nib"
+                    placeholder="Masukkan NIB"
+                    :required="true"
+                />
+            <span class="mb-2 mt-3 flex items-center justify-between gap-4">
+                <x-input
+                    icon="fa-solid fa-envelope"
+                    label="Email"
+                    type="email"
+                    name="email"
+                    placeholder="Masukkan Email"
                     :required="true"
                 />
                 <x-input
@@ -64,6 +49,26 @@
                     placeholder="Masukkan Nomor Telepon"
                     :required="true"
                 />
+            </span>
+            <span class="mb-2 mt-3 flex items-center justify-between gap-4">
+                <x-input
+                    icon="fa-solid fa-globe"
+                    label="Website"
+                    type="text"
+                    name="website"
+                    placeholder="Masukkan Website"
+                    :required="true"
+                />
+                <x-select
+                    icon="fa-solid fa-toggle-on"
+                    label="Status"
+                    type="text"
+                    name="status"
+                    placeholder=""
+                    :required="true"
+                    :options="['Aktif' => 'Aktif', 'Tidak Aktif' => 'Tidak Aktif']"
+                />
+            </span>
             </div>
             <span class="flex justify-end gap-3 text-sm items-center mt-6">
                 <button type="button" class="close cursor-pointer bg-red-500 text-white px-5 py-2 rounded hover:bg-red-600 transition-all duration-300">
