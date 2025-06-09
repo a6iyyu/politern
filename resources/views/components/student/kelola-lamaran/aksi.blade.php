8c58ea72608998b38c9a5023bdc1cc1cda18a6e4
Ivansyah <ivansyahsantoso@gmail.com>
2025-06-09 09:24:14 +0700

    ⌛  Sesuaikan tampilan kelola lamaran dengan figma

--- resources/views/components/student/kelola-lamaran/aksi.blade.php
+++ resources/views/components/student/kelola-lamaran/aksi.blade.php
@@ -1,4 +1,9 @@
-<a href="" class="flex items-center gap-2 bg-[var(--primary)] text-white px-5 py-2.5 rounded-md text-xs transition-all duration-300 ease-in-out lg:hover:bg-[var(--primary)]/90">
-    <i class="fa-solid fa-pen-to-square"></i>
+<a href="" class="flex items-center gap-2 bg-[var(--blue-tertiary)] text-white px-5 py-2.5 rounded-md text-xs transition-all duration-300 ease-in-out lg:hover:bg-[var(--primary)]/90">
     <h5>Detail</h5>
+</a>
+<a href="" class="flex items-center gap-2 bg-[var(--yellow-tertiary)] text-white px-5 py-2.5 rounded-md text-xs transition-all duration-300 ease-in-out lg:hover:bg-yellow-500">
+    <h5>Edit</h5>
+</a>
+<a href=""class="flex items-center gap-2 bg-[var(--red-tertiary)] text-white px-5 py-2.5 rounded-md text-xs transition-all duration-300 ease-in-out lg:hover:bg-yellow-500">
+    <h5>Hapus</h5>
 </a>
