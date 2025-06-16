5fc50d5a15f69dc6aebca3b8a40e585bc6648d43
Maulana Rengga Ramadan <mlnrengga@gmail.com>
2025-06-16 11:40:49 +0700

    🔥 menghapus button detail di pengajuan

--- resources/views/components/admin/pengajuan-magang/aksi.blade.php
+++ resources/views/components/admin/pengajuan-magang/aksi.blade.php
@@ -2,6 +2,3 @@
 <a href="javascript:void(0)" data-id="{{ $pengajuan->id_pengajuan_magang }}" class="detail cursor-pointer mr-1 rounded px-4 py-2 text-white text-xs font-medium transition-all duration-300 ease-in-out bg-[var(--blue-tertiary)] lg:hover:bg-blue-700">
     Detail
 </a>
-<a href="javascript:void(0)" data-id="{{ $pengajuan->id_pengajuan_magang }}" class="edit cursor-pointer mr-1 rounded px-4 py-2 text-white text-xs font-medium transition-all duration-300 ease-in-out bg-[var(--yellow-tertiary)] lg:hover:bg-yellow-500">
-    Edit
-</a>
