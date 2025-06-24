6e9bafcec49ae0249a327ab37aa27225201b475f
Ayleen Ruhul Qisthy <qisthyayleen@gmail.com>
2025-06-24 14:13:43 +0700

    Revisi After Demo: Fix Kelola Lamaran Pengajuan

--- resources/views/components/student/kelola-lamaran/tabel.blade.php
+++ resources/views/components/student/kelola-lamaran/tabel.blade.php
@@ -1,6 +1,17 @@
 <h4 class="cursor-default my-2 text-base font-semibold text-[var(--primary)]">
     Riwayat Lamaran
 </h4>
+@if (session('success'))
+    <h5 class="mb-4 p-4 cursor-default rounded-lg bg-emerald-50 border border-emerald-500 list-disc list-inside text-sm text-emerald-500">
+        {{ session('success') }}
+    </h5>
+@elseif ($errors->any())
+    <ul class="mb-7 p-4 cursor-default rounded-lg bg-red-50 border border-red-500 list-disc list-inside text-sm text-red-500">
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+@endif
 @include('components.student.kelola-lamaran.filter')
 <x-table
     :headers="['No', 'Nama Perusahaan', 'Posisi Magang', 'Periode Magang', 'Tanggal Pengajuan',  'Status', 'Aksi']"
