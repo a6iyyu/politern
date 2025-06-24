a044e76509ee2e2668820ac88627e1aa1a4cef1c
Ayleen Ruhul Qisthy <qisthyayleen@gmail.com>
2025-06-24 10:09:44 +0700

    perbaiki konfigurasi Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,26 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
+import { resolve } from 'path';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
+            input: ['resources/js/app.js'],
             refresh: true,
         }),
         tailwindcss(),
     ],
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'resources/js/app.js')
+            }
+        }
+    },
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, 'resources/js')
+        }
+    }
 });
